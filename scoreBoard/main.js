f1a73c3ef7926bfd0910d1026525e42fee834600
MarilenaRoque <marilenas@id.uff.br>
2020-11-04 19:38:11 -0300

    Set the function to return the id of the game set

--- scoreBoard/main.js
+++ scoreBoard/main.js
@@ -3,8 +3,10 @@ const name = JSON.stringify({
 	"name": "My cool new game" 
 });
 
+
+
 const setGame = (name) => {
-    fetch("https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/", {
+    let p = fetch("https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/", {
         mode: 'cors',
         headers: {
             'Content-Type': 'application/json',
@@ -13,7 +15,22 @@ const setGame = (name) => {
         body: name
     }).then( function (response) {
         return response.json();
-    }).then( function (response) {
-        console.log(response);
+    }).then(function (response) {
+        return response.result.split(' ')[3]
     })
+    return p
 }
+
+
+// const scoreList = () => {
+
+// }
+
+// const idGame = setGame(name);
+
+// idGame.then(function (response) {
+//     console.log(response)
+// })
+
+
+
